ee5d1c44ee1659b6f7b78de4f9475807260b8e60
Milos777 <milos.bjelcevic@diwanee.com>
2017-02-11 17:08:11 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,24 +2,24 @@ $(document).ready(function(){
 $('.content').css('opacity', 0);
 $('.content').waypoint(function() {
     $('.content').addClass('animated fadeInUp');
-}, { offset: '70%' });
+}, { offset: '90%' });
 
 
 $('.left').css('opacity', 0);
 $('.left').waypoint(function() {
     $('.left').addClass('animated fadeInLeft');
-}, { offset: '70%' });
+}, { offset: '90%' });
 
 $('.middle').css('opacity', 0);
 $('.middle').waypoint(function() {
     $('.middle').addClass('animated fadeIn');
-}, { offset: '70%' });
+}, { offset: '90%' });
 
 
 $('.right').css('opacity', 0);
 $('.right').waypoint(function() {
     $('.right').addClass('animated fadeInRight');
-}, { offset: '70%' });
+}, { offset: '90%' });
 
 
 $(".menu-mobile").on('touch click' , function() {
